c8dcb15aeae8766aa2ce814867c3f2a3f50102b1
Jonathan Lee <jonleesongyang@gmail.com>
2022-01-12 23:40:33 +0000

    exercise 3 checkpoint

--- public/js/part1.js
+++ public/js/part1.js
@@ -1,4 +1,10 @@
 // js script for part 1
 
-// Task 1
+// Exercise 1
 console.log('d3.version:', d3.version);
+let task1 = d3.select('.task1').append('span')
+task1.text(d3.version);
+
+// Exercise 2
+d3.selectAll('.task2').style('color', 'blue');
+console.log('d3.version:', 'reach');
