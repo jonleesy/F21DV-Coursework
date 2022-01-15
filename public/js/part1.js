c73482f702ad975a7eb379dc0114af150a9f0e25
jonleesyisawesome <jonleesongyang@gmail.com>
2022-01-15 20:17:57 +0000

    up tp exe 10

--- public/js/part1.js
+++ public/js/part1.js
@@ -1,23 +1,23 @@
 // js script for part 1
 
 // Exercise 1
-// Adding a span after div with class: "task1".
+// Adding a span after div with class: 'task1'.
 // d3 select then is stored in const task1.
 let task1 = d3.select('.task1').append('span');
 // Then setting the text to show version of d3
 task1.text(d3.version);
 
 // Exercise 2
-// Out of the 3 divs, first and last was classed as "task2".
-// Selecting those that were named "task2" and changing the 
+// Out of the 3 divs, first and last was classed as 'task2'.
+// Selecting those that were named 'task2' and changing the 
 // font colour to blue
 d3.selectAll('.task2').style('color', 'blue');
 
 // Exercise 3
-// Loop for 10 divs, where each has "task3" classed div as
+// Loop for 10 divs, where each has 'task3' classed div as
 // parent div.
 for (let i=1; i < 11; i++) {
-    d3.select('.task3') // Select "task3".
+    d3.select('.task3') // Select 'task3'.
         .append('div') // Add a div each iteration.
         .attr('class', function() { // For each div, we assign a unique class name.
             return 'task3id' + i.toString()
@@ -35,14 +35,14 @@ for (let i=1; i < 11; i++) {
 };
 
 // Exercise 4
-// d3 to select "task4button" and add an event listener that listens
+// d3 to select 'task4button' and add an event listener that listens
 // for the click event.
 d3.select('#task4button').on('click', function() {
     // Conditioned d3 modificaton of the first div.
     // If the first div has its original value, modify and
-    // replace value with "start" and colour purple.
-    // Else, if its purple with value "start", modify and
-    // replace value with original "1"
+    // replace value with 'start' and colour purple.
+    // Else, if its purple with value 'start', modify and
+    // replace value with original '1'
     // Modification is on: div's BG color, text colour, and
     // the div's text.
     if (d3.select('.task3id1').text() == '1') {
@@ -59,27 +59,27 @@ d3.select('#task4button').on('click', function() {
 });
 
 // Exercise 5
-// Practice on chained commands. Select "task5" div, then add another div
-// that says "Hello World!" and style it in green text.
+// Practice on chained commands. Select 'task5' div, then add another div
+// that says 'Hello World!' and style it in green text.
 d3.select('.task5').append('div').text('Hello World!')
     .style('color','green');
 
 // Exercise 6
-// Add "color" in otherdata
+// Add 'color' in otherdata
 let otherdata = [{name:'test', val:1, color:'red'},
                 {name:'other', val:2, color:'green'}, 
                 {name:'b',     val:3, color:'blue'}];
-// Select "container6" > "answercenter" > all its child divs,
-// then use the data method to instantiate "otherdata" as the data for d3, then using the
+// Select 'container6' > 'answercenter' > all its child divs,
+// then use the data method to instantiate 'otherdata' as the data for d3, then using the
 // .text() function, add a nested function that returns the wanted name + colour 
 // for text of said div. Then, using the .style() funciton, we style the text to its respective colour
-d3.select('.container6').select('.answercenter').selectAll("div")
+d3.select('.container6').select('.answercenter').selectAll('div')
     .data(otherdata)
     .text(function (d, i) {
-        // console.log("d.name: " + d.name);
-        // console.log("d.val: " + d.val);
-        // console.log("i: " + i); 
-        // console.log("this: " + this);
+        // console.log('d.name: ' + d.name);
+        // console.log('d.val: ' + d.val);
+        // console.log('i: ' + i); 
+        // console.log('this: ' + this);
         return i+1 + '. cont: ' + d.name + '; color: ' + d.color; // return value is used to set the 'text'
     })
     .style('color', function (d, i) {
@@ -123,7 +123,7 @@ const selected8 = d3.select('.container8').select('.answercenter')
 selected8.data(data8).enter()
         .append('span')
         .text(function (d, i) {
-            return d
+            return d;
         })
         .style('color', 'white')
         .style('background-color', function (d, i) {
@@ -146,7 +146,7 @@ selected8.data(data8).enter()
 const createdivs = function(taskid, height) {
     d3.select('body').append('div')
         .attr('class', 'container' + taskid)
-        .attr('height', height + 'px')
+        .style('height', height + 'px')
         .attr('position', 'relative')
         .attr('display', 'flex');
     d3.select('.container' + taskid).append('div')
@@ -156,34 +156,107 @@ const createdivs = function(taskid, height) {
 };
 
 // Exercise 9
-createdivs(9, 200);
+// Initialising Divs for task 9.
+createdivs(9, 265);
 // Importing the given titanic data.
 let data9 = 'https://raw.githubusercontent.com/dsindy/kaggle-titanic/master/data/test.csv';
-d3.csv(data9, function(d) {
-    let selection9 = d3.select('container9').select('answercenter')
-    let countObj = {}
+// Using a predefined variable for d3 in queston 9.
+let selection9 = d3.select('.container9').select('.answercenter');
+d3.csv(data9).then(function(data) {
+    // Local Variables for <p> print statements.
+    let mr = 0;
+    let mrs = 0;
+    let other = 0;
+    let male = 0;
+    let female = 0;
+    // Iterate thrugh every entry of data.
     data.forEach(function(d) {
-        var name = d.name
-        if (name.substring(0, 2) === 'Mr') {
-            if (countObj[male] === undefined) {
-                countObj[male] = 1
-            } else {
-                countObj[male] = countObj[male] + 1
-            }
-        } else if (name.substring(0, 3) === 'Mrs') {
-            if (countObj[female] === undefined) {
-                countObj[female] = 1
-            } else {
-                countObj[female] = countObj[female] + 1
-            }
+        // Using local variables to store names and sex for if statements.
+        let n = d.Name;
+        let s = d.Sex;
+        // Add 1 for each entries that have 'Mr.' and 'Mrs.' respectively. 
+        if (n.includes('Mr.')) {
+            mr ++;
+        } else if (n.includes('Mrs.')) {
+            mrs ++;
         } else {
-            if (countObj[others] === undefined) {
-                countObj[others] = 1
-            } else {
-                countObj[others] = countObj[others] + 1
-            }
+            other ++;
+        }
+        // To check and count entries to see how many male and female on board.
+        if (s === 'male') {
+            male ++;
+        } else if (s == 'female') {
+            female ++;
         }
     });
-    selection9.append('p')
-        .text(countObj.toString())
+    // Using results to append paragrpahs for this div.
+    selection9.append('p').text('A total of ' + mr.toString() + ' passengers identifies as Mr.');
+    selection9.append('p').text('A total of ' + mrs.toString() + ' passengers identifies as Mrs.');
+    selection9.append('p').text('A total of ' + other.toString() + ' passengers identifies as neither.');
+    selection9.append('p').text('there are ' + male + ' male and ' + female + ' female passengers.');
 });
+
+// Exercise 10
+// Initialising divs for task 10.
+createdivs(10, 260);
+// Reading in the data
+let data10 = 'https://raw.githubusercontent.com/akmand/datasets/master/heart_failure.csv';
+// Storing data into the range-variable.
+d3.csv(data10).then(function(data) {
+    // Create local variables for 4 ranges of age.
+    let young = 0;
+    let mid = 0;
+    let old = 0;
+    let veryold = 0;
+    data.forEach(function(d) {
+        // Checking if heart failure falls into age range.
+        if (parseInt(d.age) <= 30) {
+            young ++;
+        } else if (parseInt(d.age) <= 40) {
+            mid ++;
+        } else if (parseInt(d.age) <= 60) {
+            old ++; 
+        } else {
+            veryold ++;
+        };
+    });
+    // Select the correct divs, then select all <p> (which are not present), 
+    // and by using enter(), we create new <p>s that were not present. Once
+    // done, using the .text() and function (d,i) nested function, we obtain
+    // the right text to display for all 
+    d3.select('.container10').select('.answercenter').selectAll('p')
+        .data([0, young, mid, old, veryold]).enter().append('p')
+        .text(function(d, i) {
+            let msg = d.toString() + ' people with age ';
+            if (i == 1) {
+                msg = msg + '1-31';
+            } else if (i == 2) {
+                msg = msg + '31-40';
+            } else if (i == 3) {
+                msg = msg + '41-60';
+            } else if (i == 4) {
+                msg = msg + '61-100';
+            }
+            msg = msg + ' have heart disease.';
+            return msg;
+        });
+});
+
+// Exercise 11
+// Systematically creating divs for new exercise
+createdivs(11, 200)
+// Generalised d3 svg div selection
+let svg11 = d3.select('.container11').select('.answercenter');
+// Create SVG element
+svg11.append('svg')
+    .attr('width', 200)
+    .attr('height', 200)
+    .style('border', '2px solid green')
+ //Create line element inside SVG
+svg11.append("line")
+    .attr("x1", 0)
+    .attr("x2", 40)
+    .attr("y1", 0)
+    .attr("y2", 40)
+    .attr("stroke-width", 2)
+    .attr("stroke", "blue")
