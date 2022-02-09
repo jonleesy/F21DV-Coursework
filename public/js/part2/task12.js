477bdda4fce83247f152858598031519b4d01f1a
Jonathan Lee <jonleesongyang@gmail.com>
2022-02-09 16:55:06 +0000

    Task 12

---
+++ public/js/part2/task12.js
@@ -0,0 +1,39 @@
+// .js script for exercise:
+const ex = 12;
+
+// Imports of functions.
+import { createAnswerDiv } from '../functions.js';
+
+// Creating base <div>s systematically
+createAnswerDiv(ex);
+
+// Svg Constants
+const svgLength = 300, margin = 10;
+
+const data = [10, 110, 210]
+
+const svg = d3.select('.answer-grid')
+                .append('svg')
+                .attr('width', svgLength)
+                .attr('height', svgLength)
+                .style('border', '2px solid black');
+
+const bars = svg.selectAll('rect')
+                .data(data)
+                .join('rect')
+                    .attr('fill', 'blue')
+                    .attr('x', d => d)
+                    .attr('y', margin)
+                    .attr('height', 0)
+                    .attr('width', svgLength/3 - margin * 2);
+
+update();
+
+function update() {
+    // Forward Transition
+    bars.transition()
+        .ease(d3.easeExp)
+        .duration(2000)
+        .delay((_, i) => i * 1900)
+        .attr('height', (_, i) => (svgLength/3)*(i + 1) - margin*2)
+}
