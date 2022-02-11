3b438cf8e1ae63ea1d7a2a79edd596301a73ffa5
Jonathan Lee <jonleesongyang@gmail.com>
2022-02-11 02:38:54 +0000

    task 23

--- public/js/part2/task23.js
+++ public/js/part2/task23.js
@@ -1,5 +1,5 @@
 // .js script for exercise:
-const ex = 21;
+const ex = 23;
 
 // Imports of functions.
 import { createAnswerDiv } from '../functions.js';
@@ -8,24 +8,8 @@ import { createAnswerDiv } from '../functions.js';
 createAnswerDiv(ex);
 
 // Creating the data
-const data1 = [
-    {group:'A', value:5, color:'red'},
-    {group:'B', value:20, color:'red'},
-    {group:'C', value:9, color:'red'}
-];
-
-const data2 = [
-    {group:'A', value:10, color:'green'},
-    {group:'B', value:2, color:'green'},
-    {group:'C', value:22, color:'green'},
-    {group:'D', value:16, color:'green'}
-];
-
-const data3 = [
-    {group:'A', value:6, color:'blue'},
-    {group:'B', value:18, color:'blue'},
-    {group:'C', value:11, color:'blue'}
-];
+const data1 = await d3.csv('../../data/part1/task23.csv');
+const data2 = await d3.csv('../../data/part2/task23.csv');
 
 const dataColours = ['red', 'green', 'blue'];
 
@@ -44,12 +28,11 @@ const svg = d3.select('.answer-grid')
                             .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 // Define the Axes
-let horScale = d3.scaleBand()
+let horScale = d3.scaleLinear()
                     .range([0, width])
-                    .domain(data1.map(d => d.group))
-                    .padding(0.2);
+                    .domain(d3.extent(data1, d => d.x));
 let verScale = d3.scaleLinear()
-                    .domain([0, 22])
+                    .domain(d3.extent(data1, d => d.y))
                     .range([height, 0]);
 
 // Adding the Axes
@@ -61,21 +44,34 @@ svg.append('g')
     .attr('class', 'top-axis')
     .call(d3.axisTop(horScale))
 svg.append('g')
-    .attr('class', 'myYAxis')
+    .attr('class', 'left-axis')
     .call(d3.axisLeft(verScale));
 svg.append('g')
     .attr('transform', `translate(${width}, 0)`)
-    .attr('class', 'myYAxis')
+    .attr('class', 'right-axis')
     .call(d3.axisRight(verScale));
+
+const line = d3.line()
+                .x(d => horScale(d.x))
+                .y(d => verScale(d.y))
+
+// Add the initial path.
+svg.append('path')
+    .attr('class', 'line')
+    .attr('fill', 'none')
+    .attr('stroke', 'black')
+    .attr('d', line(data1))
 
 // Bar Chart Function. Clears and add.
 function update(data) {
 
     // Re-Define the Axes
-    horScale = d3.scaleBand()
+    horScale = d3.scaleLinear()
                 .range([0, width])
-                .domain(data.map(d => d.group))
-                .padding(0.2);
+                .domain(d3.extent(data, d => d.x));
+    verScale = d3.scaleLinear()
+                .domain(d3.extent(data, d => d.y))
+                .range([height, 0]);
 
     // Re-Call the Axes.
     d3.selectAll('.bottom-axis')
@@ -88,42 +84,22 @@ function update(data) {
             .duration(400)
             .ease(d3.easeLinear)
             .call(d3.axisTop(horScale))
-
-    // Using a variable to implement a .merge() function.
-    const u = svg.selectAll('rect')
-                .data(data)
-
-    u.enter()
-        .append('rect')
-            .on('mouseover', function(e, d) {                        
-                    svg.append('text')
-                    .attr('x', horScale(d.group) + margin.left/(data.length))
-                    .attr('y', verScale(d.value)/2 + height/2)
-                    .attr('class', 'temp-text')
-                    .attr("dy", ".35em")
-                    .text(d.value);
-            })
-            .on('mouseout', function(_, i) {
-                d3.select('.temp-text')
-                    .remove()
-            })
-        .merge(u)
-        .transition()
-            .duration(1000)
-                .attr('x', d => horScale(d.group))
-                .attr('y', d => verScale(d.value))
-                .attr('width', horScale.bandwidth())
-                .attr('height', d => height - verScale(d.value))
-                .attr('fill', d => d.color);
-
-    // Remove the extra bar.
-    u.exit()
+    d3.selectAll('.left-axis')
         .transition()
             .duration(400)
             .ease(d3.easeLinear)
-            .attr('y', height)
-            .attr('height', 0)
-            .remove();
+            .call(d3.axisLeft(verScale))
+    d3.selectAll('.right-axis')
+        .transition()
+            .duration(400)
+            .ease(d3.easeLinear)
+            .call(d3.axisRight(verScale))
+
+    // Update the svg line data.
+    svg.selectAll('.line').transition()
+            .duration(400)
+            .ease(d3.easeLinear)
+            .attr('d', line(data))
 }
 
 // Opening a data upon launch.
@@ -135,7 +111,7 @@ const buttonGrid = d3.select('.answer-grid')
                         .attr('class', 'inner-grid');
 
 // Button names for div population.
-const buttonData = ['data1', 'data2', 'data3'];
+const buttonData = ['data1', 'data2'];
 
 // Add buttons (divs).
 buttonGrid.selectAll('div')
@@ -171,20 +147,8 @@ d3.select('.data2')
         d3.select(this)
         .style('background-color', 'green')
     });
-d3.select('.data3')
-    .on('click', function() {
-        update(data3);
-    })
-    .on('pointerover', function() {
-        d3.select(this)
-        .style('background-color', 'lightblue')
-    })
-    .on('mouseleave', function() {
-        d3.select(this)
-        .style('background-color', 'blue')
-    });
 
 // Add instructions.
 d3.select('.answer-grid')
     .append('p')
-        .text('Click on data 1/2/3 to present new data.')
+        .text('Click on data 1/2 to present new data.')
