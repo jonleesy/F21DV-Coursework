5869e0abb9f286ae67f00dae5a97f1e585f4c588
Jonathan Lee <jonleesongyang@gmail.com>
2022-02-15 16:09:33 +0000

    task 6 report update

--- public/js/part2/task6.js
+++ public/js/part2/task6.js
@@ -7,6 +7,7 @@ import { createAnswerDiv } from '../functions.js';
 // Creating base <div>s systematically
 createAnswerDiv(ex);
 
+// Add original div.
 d3.select('.answer-grid')
     .append('div')
         .style('width', 'auto')
@@ -14,23 +15,28 @@ d3.select('.answer-grid')
         .style('background-color', 'blue')
         .text('1');
 
+// Add Transition.
 d3.select('.answer-grid div')
     .on('click', function() {
         d3.select(this)
+            // Ori -> 2(red)
             .transition()
                 .duration(1000)
                 .style('background-color', 'red')
                 .text('2')
+            // 2(red) -> 3(green)    
             .transition()
                 .duration(1000)
                 .style('background-color', 'green')
                 .text('3')
+            // 3(green) -> Ori
             .transition()
                 .duration(1000)
                 .style('background-color', 'blue')
                 .text('1')
     });
 
+// Add Instructions.
 d3.select('.answer-grid')
     .append('p')
     .text('Click on colour box to change colour.')
